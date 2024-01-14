bcb374b6c6bc9467e115cf6f4277645e40bd59df
Jean2016Gv <jeanmarievasconcelos4@gmail.com>
2024-01-13 21:32:06 -0400

    Task 3

--- server.js
+++ server.js
@@ -6,8 +6,14 @@ const http = require('http');
 const sqlite3=require('sqlite3').verbose();
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const Recaptcha = require('express-recaptcha').RecaptchaV2;
+const recaptcha = new Recaptcha('6Le6p0spAAAAAKDIQVrcTlvwmMy2PM4i6oWhKP1-', '6Le6p0spAAAAAG4WmCFqc51ucRpcPtuHaJeZfeGC');
+const axios = require('axios');
+app.use(recaptcha.middleware.verify);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
+app.use(express.json());
+app.set('trust proxy', true);
 
 app.use(session({
     secret: 'secretkey',
@@ -15,34 +21,119 @@ app.use(session({
     saveUninitialized: false
   }));
   app.get('/', function(req, res) {
-    const sqlProd = "SELECT * FROM Productos ORDER BY id";
-    const sqlCat = "SELECT * FROM Categorias ORDER BY id";
-    const sqlIma = "SELECT * FROM Imagenes ORDER BY id";
-    db.all(sqlProd, [], (errProductos, rowsProductos) => {
+    let sql = 'SELECT Productos.ID, Productos.Nombre, Productos.precio, Productos.Codigo, Productos.Descripcion, Productos.Marca, Productos.Color, Categorias.Nombre AS CategoriaNombre, Imagenes.URL, Imagenes.Destacado FROM Productos INNER JOIN Categorias ON Productos.Categoria_ID = Categorias.ID INNER JOIN Imagenes ON Productos.ID = imagenes.producto_id where 1=1';
+
+    db.all(sql, [], (errProductos, rowsProductos) => {
       if (errProductos) {
         return console.error(errProductos.message);
       } else {
-        db.all(sqlCat, [], (errCategorias, rowsCategorias) => {
-          if (errCategorias) {
-            return console.error(errCategorias.message);
-          } else {
-            db.all(sqlIma, [], (errImagenes, rowsImagenes) => {
-              if (errImagenes) {
-                return console.error(errImagenes.message);
-              } else {
-                res.render("inicio", { Productos: rowsProductos, Categorias: rowsCategorias, Imagenes: rowsImagenes });
+               if (req.session.user || req.session.admin) {
+                res.render("inicio", { Productos: rowsProductos, url: '/cerrarsesion' });
+               } else {
+                res.render("inicio", { Productos: rowsProductos, url: '/registro.Clientes' });
+               }
               }
-            });
-          }
-        });
-      }
     })
   });
+app.get('/registro.Clientes', recaptcha.middleware.render, (req, res)  =>{
+  if (req.session.admin) {
+    res.redirect('/administrador')
+  } else {
+    if (req.session.user == true) {
+   res.redirect('/')
+  } else {
+    res.render('registro.Clientes.ejs' , { captcha: res.recaptcha })
+  }
+  }
+
+  
+
+});
+app.post('/registro.Clientes' , (req, res) =>{
+  const { Nombre, Correo, Contraseña} = req.body;
+  const sql= "SELECT correo FROM Registro WHERE correo = ?" ;
+  recaptcha.verify(req, (error, recaptchadatos) => { 
+    if (error) {
+      console.error(error);
+      return res.status(500).send('Error en el servidor');
+    } else {
+      db.get(sql, [Correo], (err, row) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send('Error en el servidor');
+        }
+        
+        if (row) {
+          return res.redirect('/registro.Clientes');
+        }
+    
+        const sqlCliente = "INSERT INTO Registro (Nombre, Correo, Contraseña) VALUES (?, ?, ?)";
+        db.run(sqlCliente, [Nombre, Correo, Contraseña], (err) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).send('Error en el servidor');
+          } else {
+            req.session.user = true;
+            req.session.username = Correo;
+            req.session.admin = false;
+            return res.redirect('/');
+          }
+        });
+      });
+    }
+
+  });
+});
+
+
+app.get('/inicioSesionClientes', (req,res) =>{
+  if (req.session.admin == true ){
+    res.redirect('/administrador')
+
+  }else{
+    if (req.session.user == true){
+      res.redirect ('/')
+    }else{
+      res.render('inicioSesionClientes')
+    }
+  }
+ });
+
+
+
+app.post('/inicioSesionClientes', (req, res) => {
+  const { correo, contraseña } = req.body;
+
+    const sql ='SELECT * FROM Registro WHERE Correo = ? AND Contraseña = ?';
+  db.get(sql, [correo, contraseña], (err, row) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error en el servidor');
+    }
+
+    if (row) {
+      req.session.username = correo;
+      req.session.user = true;
+      req.session.admin = false;
+      res.redirect('/');
+    } else {
+      res.redirect('/inicioSesionClientes');
+    }
+  });
+});
+
+
+
 app.get('/login', (req, res) => {
   if (req.session.admin == true) {
     res.redirect("/administrador");
   } else {
-    res.render('login');
+    if (req.session.user == true){
+      res.redirect ('/')
+
+    }else{
+      res.render('login');
+    }
   }
        
   });
@@ -52,6 +143,7 @@ app.get('/login', (req, res) => {
   const contra = process.env.PASSWORD;
   if (usuario == username && contraseña == contra) {
     req.session.admin = true;
+    req.session.user = false;
     res.redirect('/administrador');
   } else {
     res.render('login')
@@ -59,7 +151,7 @@ app.get('/login', (req, res) => {
 });
 app.get('/cerrarsesion', (req, res) => {
     req.session.destroy();
-      res.redirect('/login');
+      res.redirect('/');
    });
     app.get('/administrador', (req, res) => {
     if (req.session.admin) {
@@ -416,85 +508,46 @@ app.get('/cerrarsesion', (req, res) => {
     res.render('carrito');
 });
 
-app.get('/info/:id', (req, res) => {
-  const id = req.params.id;
-  const sqlProd = "SELECT * FROM productos WHERE id = ?";
-  const sqlIma = "SELECT * FROM imagenes WHERE producto_id = ?";
-  const sqlCat = "SELECT * FROM categorias WHERE id IN (SELECT categoria_id FROM productos WHERE id = ?)";
-  
-  db.get(sqlProd, id, (err, productos) => {
-    if (err) {
-      console.error(err);
-      console.log("Error al obtener el producto.");
-    } else {
-      db.get(sqlIma, id, (err, imagenes) => {
-        if (err) {
-          console.error(err);
-          console.log("Error al obtener las imágenes.");
-        } else {
-          db.get(sqlCat, id, (err, categorias) => {
-            if (err) {
-              console.error(err);
-              console.log("Error al obtener las categorías.");
-            } else {
-              res.render("info", { productos, imagenes, categorias, id });
-            }
-          });
-        }
-      });
-    }
-  });
-});
-
 app.post('/buscar', (req, res) => {
   const { Nombre, Categoria, Descripcion, Marca, Color } = req.body;
- let sql = "SELECT * FROM Productos WHERE 1=1";
-  const sqlIma = "SELECT * FROM Imagenes ORDER BY id";
-  const sqlCat = "SELECT * FROM Categorias ORDER BY id";
+  let sql = 'SELECT Productos.ID, Productos.Nombre, Productos.precio, Productos.Codigo, Productos.Descripcion, Productos.Marca, Productos.Color, Categorias.Nombre AS CategoriaNombre, Imagenes.URL, Imagenes.Destacado FROM Productos INNER JOIN Categorias ON Productos.Categoria_ID = Categorias.ID INNER JOIN Imagenes ON Productos.ID = imagenes.producto_id where 1=1';
+
   const params = [];
   if (Nombre) {
-    sql += " AND Nombre LIKE '%' || ? || '%'";
+    sql += " AND Productos.Nombre LIKE '%' || ? || '%'";
     params.push(Nombre);
   }
 
   if (Categoria) {
-    sql += " AND Categoria_ID = ?";
+    sql += " AND CategoriaNombre= ?";
     params.push(Categoria);
   }
 
   if (Descripcion) {
-    sql += " AND descripcion LIKE '%' || ? || '%'";
+    sql += " AND Productos.Descripcion LIKE '%' || ? || '%'";
     params.push(Descripcion);
   }
 
   if (Marca) {
-    sql += " AND Marca LIKE '%' || ? || '%'";
+    sql += " AND Productos.Marca LIKE '%' || ? || '%'";
     params.push(Marca);
   }
 
   if (Color) {
-    sql += " AND Color LIKE '%' || ? || '%'";
+    sql += " AND Productos.Color LIKE '%' || ? || '%'";
     params.push(Color);
   }
 
   db.all(sql, params, (err, rows) => {
     if (err) {
       return console.error(err.message);
-    } else {
-      db.all(sqlIma, (err, Imagenes) => {
-        if (err) {
-          return console.error(err.message);
-        } else {
-          db.all(sqlCat, (err, Categorias) => {
-            if (err) {
-              return console.error(err.message);
             } else {
-              res.render("inicio", { Productos: rows, Imagenes, Categorias  });
+              if (req.session.user || req.session.admin) {
+                res.render("inicio", { Productos: rows, url: '/cerrarsesion' });
+               } else {
+                res.render("inicio", { Productos: rows, url: '/registro.Clientes' });
+               } 
             }
-          });
-        }
-      });
-    }
   });
 });
 app.get('/producto/:id', (req, res) => {
@@ -532,10 +585,153 @@ app.get('/producto/:id', (req, res) => {
   });
 });
 
+//direccion payment para el formulario
+
+app.get('/pago/:id', (req, res) => {
+  const id = req.params.id;
+  const username = req.session.username;
+  if (!req.session.user) {
+    res.redirect('/registro.Clientes');
+  } else if (req.session.admin) {
+    res.redirect('/administrador');
+  } else {
+    const sqlProd = "SELECT * FROM productos WHERE id = ?";
+    const sqlIma = "SELECT * FROM imagenes WHERE producto_id = ?"
+    const sqlReg = "SELECT * FROM Registro WHERE correo = ?"
+    db.get(sqlProd, id, (err, rowsProductos) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      
+      if (!rowsProductos) {
+        res.status(404).send("Producto no encontrado.");
+        return;
+      }
+  
+      db.get(sqlIma, id, (err, rowsImagenes) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        db.get(sqlReg, username, (err, rowsRegistro) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          res.render('pago', { productos: rowsProductos, clientes: rowsRegistro, imagenes: rowsImagenes, id, username });
+        });
+      });
+    });
+  }  
+});
+
+app.post('/payments', async (req, res) => {
+  const ip_cliente = req.ip;
+  const {
+    cvv,
+    'expiration-month': expirationMonth,
+    'expiration-year': expirationYear,
+    name: fullName,
+    "card-number": tarjeta,
+    precio,
+    id: idProducto,
+    idCliente: idCliente,
+    cantidad
+  } = req.body;
+  const precioProducto = (precio * cantidad).toFixed(2);
+  const payment = {
+    amount: precioProducto,
+    'card-number': tarjeta,
+    cvv,
+    'expiration-month': expirationMonth,
+    'expiration-year': expirationYear,
+    'full-name': fullName,
+    currency: 'USD',
+    description: idProducto,
+    reference: idCliente
+  };
+
+  try {
+    const response = await axios.post('https://fakepayment.onrender.com/payments', payment, {
+      headers: {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJkYXRlIjoiMjAyNC0wMS0xM1QyMTo1MTowMy42NjhaIiwiaWF0IjoxNzA1MTgyNjYzfQ.axESJcjQ06PKq3gQEUJhkuDNNztHGNnLAdnxzoyEfrA'
+      }
+    });
+    const datos = JSON.parse(JSON.stringify(response.data));
+    const transactionId = datos.data.transaction_id;
+    const amount = datos.data.amount;
+    const date = datos.data.date;
+    const reference = datos.data.reference;
+    const description = datos.data.description;
+    await new Promise((resolve, reject) => {
+      db.run("INSERT INTO Compra (Transaccion_ID, Cantidad, Total_Pagado, Fecha, IP_Cliente, Cliente_ID, Producto_ID) VALUES(?,?,?,?,?,?,?)",
+        [transactionId, cantidad, amount, date, ip_cliente, reference, description],
+        (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+    });
+    req.session.datos = datos;
+    res.redirect("/transaccion");
+  } catch (error) {
+    res.render('transaccion', { error, datos : null, idProducto });
+  }
+});
+app.get('/transaccion', (req, res) => {
+  if (!req.session.user) {
+    res.redirect("/");
+  } else if (req.session.admin) {
+    res.redirect("/administrador");
+  } else {
+    const datos = req.session.datos;
+
+    res.render('transaccion', { datos, error: null });
+  
+    delete req.session.datos;
+  }
+});
+app.get('/administrador/clientes', (req, res) => {
+  if (!req.session.admin) {
+    res.redirect("/login");
+  } else {
+    const sql = 'SELECT * FROM Registro'
+    db.all(sql, [], (err, registro) => {
+      if (err) {
+        return console.error(err.message);
+      } else {
+        res.render("adminRegistro", { registro });
+      }
+    });
+  }
+});
+app.get('/administrador/transacciones', (req, res) => {
+  if (!req.session.admin) {
+    res.redirect("/login");
+  } else {
+    const sqlCompra = 'SELECT * FROM compra';
+
+    db.all(sqlCompra, [], (err, compra) => {
+      if (err) {
+        return console.error(err.message);
+    
+              } else {
+                res.render("adminTransaccion", { compra });
+      }
+    });
+  }
+});
 
    app.get('/*', function(req, res) {
     res.redirect('/');
   });
+
+
+
+
 
 
 app.listen(8000);
@@ -590,6 +786,30 @@ const sql_prdt = `CREATE TABLE IF NOT EXISTS Productos(ID INTEGER PRIMARY KEY AU
           console.log("Tabla Imagenes Creada");
       }
   });
+
+  //Crear Tabla Registro
+  const sql_reg = `CREATE TABLE IF NOT EXISTS Registro (ID INTEGER PRIMARY KEY AUTOINCREMENT, Nombre TEXT,Correo TEXT,Contraseña TEXT)`
+    
+    db.run(sql_reg, err => {
+        if (err) {
+            console.error(err.message);
+        }else{
+            console.log("Tabla Registro Creada");
+        }
+    });
+
+  //Crear Tabla Compra
+  const sql_cmp = `CREATE TABLE IF NOT EXISTS Compra ( Transaccion_ID TEXT PRIMARY KEY, Cantidad INTEGER, Total_Pagado FLOAT,
+   Fecha datetime, IP_Cliente TEXT, Cliente_ID TEXT, Producto_ID TEXT, FOREIGN KEY(Cliente_ID) REFERENCES Compra(ID), 
+   FOREIGN KEY(Producto_ID) REFERENCES Productos(ID) )`
+
+   db.run(sql_cmp, err => {
+    if (err) {
+        console.error(err.message);
+    }else{
+        console.log("Tabla Compra Creada");
+    }
+});
   
 
  
